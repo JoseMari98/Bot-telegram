7a1159a813225858e08604a4776a4711f422f49a
JoseMari98 <josemari-98@hotmail.com>
2019-03-27 17:54:04 +0100

    0.0.0

---
+++ private.py
@@ -0,0 +1,1 @@
+mongo = "mongodb+srv://yo:ratilla@sd-fz003.mongodb.net/test?retryWrites=true"
--- web.py
+++ web.py
@@ -2,6 +2,8 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from private import mongo
 
 url = "https://www.amazon.es/Xiaomi-Puls%C3%B3metro-sofortnachr-Resistente-meteorol%C3%B3gica/dp/B07DG3P996?pd_rd_wg=y3DHX&pd_rd_r=aa03e607-b352-417a-96d2-c16857d48a56&pd_rd_w=ozFrQ&ref_=pd_gw_ri&pf_rd_r=EM0163T4D7HX8N4KDKT3&pf_rd_p=3187c5a7-b24d-5d8f-ba5a-bd70f4f3b001"
 response = requests.get(url)
@@ -11,3 +13,14 @@ price = soup.find("span", {"id": "priceblock_ourprice"}).text
 
 
 print(price, name)
+
+client = MongoClient(mongo)
+
+db = client.test
+
+collection = db.test
+
+ejem = {"name":name,"price":price}
+id = collection.insert_one(ejem).inserted_id
+
+print(id)
